98d5bd562c9b42f25b00bcee32fd531bacca79ca
feten-bousselmi <fetenbousselmi16@gmail.com>
2026-05-15 01:05:02 +0100

    fix: resolve test compilation error

--- microservices/scoring-service/src/main/java/tn/epos/scoring_service/ScoringServiceApplication.java
+++ microservices/scoring-service/src/main/java/tn/epos/scoring_service/ScoringServiceApplication.java
@@ -2,13 +2,10 @@ package tn.epos.scoring_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-// On force Spring à scanner les packages spécifiques pour éviter les erreurs de détection
-@EnableJpaRepositories(basePackages = "tn.epos.scoring_service.repositories")
-@EntityScan(basePackages = "tn.epos.scoring_service.entities")
+// @EnableJpaRepositories(basePackages = "tn.epos.scoring_service.repositories")   //deactivated for pipline tests
+// @EntityScan(basePackages = "tn.epos.scoring_service.entities")    //deactivated for pipline tests
 public class ScoringServiceApplication {
 
 	public static void main(String[] args) {
